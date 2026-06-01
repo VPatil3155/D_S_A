31dba09302a685ccd0f3a47ba500106e1dbf674a
Vaibhav Patil <patilvaibhav8197@gmail.com>
2026-06-01 10:18:17 +0530

    Time: 0 ms (100%), Space: 43.5 MB (22.91%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,22 +1,21 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-
-        for (int i = digits.length - 1; i >= 0; i--) {
-
-            
-            if (digits[i] < 9) {
-                digits[i]=digits[i]+1;
-                return digits;
+        int carry=0;
+        int []a=new int [digits.length+1];
+        for(int i=digits.length-1;i>=0;i--){
+            if(digits[i]!=9){
+                digits[i]+=1;
+                carry=0;
+                break;
+            }else if(digits[i]==9){
+                carry=1;
+                digits[i]=0;
             }
-
-           
-            digits[i] = 0;
         }
-
-       
-        int[] newDigits = new int[digits.length + 1];
-        newDigits[0] = 1;
-
-        return newDigits;
+        if(carry==1){
+            a[0]=1;
+            digits=a;
+        }
+        return digits;
     }
 }
