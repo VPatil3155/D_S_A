40dca56da652c6b69a683376174c83eaf6b778af
Vaibhav Patil <patilvaibhav8197@gmail.com>
2026-09-08 10:52:49 +0530

    Time: 2133 ms (5.01%), Space: 41.9 MB (79.68%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -6,9 +6,7 @@ class Solution {
                 sum+=i;
             }
         }
-        if(sum==num){
-            return true;
-        }
-        return false;
+        
+        return sum==num;
     }
 }
