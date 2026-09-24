eb2c6df73fdd0430eb2e607e9cd1b937ddf7f91b
Vaibhav Patil <patilvaibhav8197@gmail.com>
2026-09-24 20:55:07 +0530

    Time: 1 ms (100%), Space: 46.5 MB (38.12%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,14 +10,13 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        // Dummy head placeholder to avoid null pointer issues when starting
+        
         ListNode dummyHead = new ListNode(0);
         ListNode current = dummyHead;
         int carry = 0;
 
-        // Loop as long as there are nodes to process or a carry digit remains
         while (l1 != null || l2 != null || carry != 0) {
-            // Extract values; use 0 if the list has run out of nodes
+           
             int val1 = 0;
             int val2 = 0;
             if(l1 != null){
@@ -26,25 +25,12 @@ class Solution {
             if(l2 != null){
                 val2 = l2.val;
             }
-            // int val1 = (l1 != null) ? l1.val : 0;
-            // int val2 = (l2 != null) ? l2.val : 0;
-
-            // Compute the current sum and updated carry
+           
             int sum = val1 + val2 + carry;
             carry = sum/10;
 
-            // int sum = val1 + val2 + carry;
-            // carry = sum / 10;
-
-            // Append the digit to the result list
-
             current.next = new ListNode(sum % 10);
             current = current.next;
-
-            // current.next = new ListNode(sum % 10);
-            // current = current.next;
-
-            // Move pointer forward in the input lists
 
             if (l1 != null){
                 l1 = l1.next;
